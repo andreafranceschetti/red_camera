b2015706a485c785879bf4f69bc11bd62541c55c
Andrea Franceschetti <andrea.franceschetti@stiga.com>
2023-08-10 11:27:22 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
+with open('requirements.txt') as file:
+  requirements = file.readlines()
 
 setup(name='red_camera',
       version='0.1',
       description='Python package to communicate from raspberry pi zero w to red camera raptor vk 8',
       author='Andrea Franceschetti',
       author_email='andrea.franceschetti.af@gmail.com',
-      install_requires=['websockets', 'gpiozero'],
+      install_requires=requirements
     #   scripts=['scripts/main']
      )
